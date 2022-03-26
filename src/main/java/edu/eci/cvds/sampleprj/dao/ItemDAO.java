24ab54cef37b8205583855da10b9c1fca0a76ee6
unknown <yesid.carrillo@labinfo.is.escuelaing.edu.co>
2022-03-26 09:28:39 -0500

    creating classes

---
+++ src/main/java/edu/eci/cvds/sampleprj/dao/ItemDAO.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.sampleprj.dao;
+
+import edu.eci.cvds.samples.entities.Item;
+import edu.eci.cvds.sampleprj.dao.PersistenceException;
+
+
+public interface ItemDAO {
+
+    public void save(Item it) throws PersistenceException;
+
+    public Item load(int id) throws PersistenceException;
+
+}
